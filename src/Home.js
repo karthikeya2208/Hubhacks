620988cdc3326a39a9beb7597d4ad7d47a0beec7
Karthikeya Kanumuri <128982279+karthikeya2208@users.noreply.github.com>
2024-10-19 13:03:42 +0000

    Added Image to the Home Page.

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
+import "./homepage.png";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to My Website</h1>
+    <div className="home-container">
+      <h1>ISANS</h1>
+      <h2>Mission & Vision</h2>
       <p>
-        This website is designed to manage client definitions and program lists.
+        Helping immigrants build a future in Nova Scotia
+        <br />A community where all can belong and grow
       </p>
+      <div className="image-wrapper">
+        <img
+          src={require("./homepage.png")} // Update with your image path
+          alt="Screenshot of the website"
+          className="home-screenshot"
+        />
+      </div>
     </div>
   );
 };
